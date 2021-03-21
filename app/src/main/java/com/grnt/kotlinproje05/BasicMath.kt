b719b228d1fd57b3cbc4bfe719e2a9deaf6eed2d
Engin Erol <engin.erol@ibb.gov.tr>
2021-03-21 14:48:37 +0300

    WeightOnPlanets

--- app/src/main/java/com/grnt/kotlinproje05/BasicMath.kt
+++ app/src/main/java/com/grnt/kotlinproje05/BasicMath.kt
@@ -1,10 +1,16 @@
 package com.grnt.kotlinproje05
 
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+
 class BasicMath {
 
     private val KILO_TO_POUND:Double = 2.2045
     private val POUND_TO_KILO:Double = 0.45359237
     private val MARS:Double = 0.38
+    private val JUPITER:Double = 2.34
+    private val VENUS:Double = 0.91
     private fun kilotoPound(kilo:Double): Double {
         return kilo * KILO_TO_POUND
     }
@@ -17,4 +23,16 @@ class BasicMath {
         var _kilo = pound * MARS
         return this.PoundToKilo(_kilo)
     }
+    fun onJupiter(kilo:Double):Double{
+        var pound = this.kilotoPound(kilo)
+        var _kilo = pound * JUPITER
+        return this.PoundToKilo(_kilo)
+    }
+    fun onVenus(kilo:Double):Double{
+        var pound = this.kilotoPound(kilo)
+        var _kilo = pound * VENUS
+        return this.PoundToKilo(_kilo)
+    }
 }
+
+
--- app/src/main/java/com/grnt/kotlinproje05/MainActivity.kt
+++ app/src/main/java/com/grnt/kotlinproje05/MainActivity.kt
@@ -2,29 +2,67 @@ package com.grnt.kotlinproje05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.CheckBox
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val MARS = "mars"
+    val JUPITER = "jupiter"
+    val VENUS = "venus"
+    var activeplanet = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Glide.with(this).load(R.drawable.planets).into(imgTitle)
 
+        if(savedInstanceState == null) return;
+        tvResult.text = savedInstanceState.getString("result").toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("result",tvResult.text.toString())
+    }
+
+    fun handleChangePlanet(v: View) {
         var personWeight = txtWeight.text
-        var basicMath:BasicMath = BasicMath();
+        var basicMath: BasicMath = BasicMath()
+        if (personWeight != null) {
+            if (!TextUtils.isEmpty(personWeight)) {
+                var result = ""
+                when (v.id) {
+                    R.id.rdyMars -> {
+                        result =
+                            basicMath.onMars(personWeight.toString().toDouble()).formatla(2)
+                                .toString()
+                    }
+                    R.id.rdyVenus -> {
+                        result =
+                            basicMath.onVenus(personWeight.toString().toDouble()).formatla(2)
+                                .toString()
+                    }
+                    R.id.rdyJupiter -> {
+                        result = basicMath.onJupiter(personWeight.toString().toDouble()).formatla(2)
+                            .toString()
+                    }
+                    else -> {
+                        result =
+                            basicMath.onMars(personWeight.toString().toDouble()).formatla(2)
+                                .toString()
+                    }
+                }
+                tvResult.text = result
 
-        btnCalc.setOnClickListener {
-            println("personWeight " + personWeight.toString().toDouble() )
-            if (personWeight != null) {
-                    var result = basicMath.onMars(personWeight.toString().toDouble())
-                    println(result)
-                    tvResult.text = result.toString()
             }
         }
-
-
     }
+
+
+    fun Double.formatla(howMany: Int) = java.lang.String.format("%.${howMany}f", this)
 
 
 }
